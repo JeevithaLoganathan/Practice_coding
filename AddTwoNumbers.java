0702749e4e7745b1431f7a5eed79b42712091787
Jeevitha Loganathan <jeevitha.loganathan@iths.se>
2024-09-05 11:26:51 +0200

    Add meaningful commit message here

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,8 +1,15 @@
-Public class AddTwoNumber{
-  public static void main(String[] args){
-    int a=2;
-    int b=3;
-    int c=a+b;
-    system.out.println("The sum of a and b is: "+c);
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println("Enter the value of a : " + a);
+
+    int b = sc.nextInt();
+    System.out.println("Enter the value of a : " + b);
+
+    int c = a + b;
+    System.out.println("The sum of a and b is: " + c);
   }
 }
